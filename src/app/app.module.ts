ea17d53e67bba0cb2652359064a9e8d0bc2a00e6
Yarden <yardenron4@gmail.com>
2018-10-08 19:09:10 +0300

    Testing simple animation css properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleAnimationComponent } from './simple-animation/simple-animation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleAnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/simple-animation/simple-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'simple-animation',
+  templateUrl: './simple-animation.component.html',
+  styleUrls: ['./simple-animation.component.scss']
+})
+export class SimpleAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
